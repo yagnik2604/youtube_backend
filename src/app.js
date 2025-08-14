84b363f36d5d530ff9fcfdbc03470f5c0af0caca
yagnik2604 <161412197+yagnik2604@users.noreply.github.com>
2025-08-14 14:14:39 +0530

    Fix CORS configuration and improve user registration route setup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import cookieParser from "cookie-parser";
 import express from "express"
-
+import cors from "cors"
 
 const app = express();
 
 
 
 app.use(cors({
-    origin: Process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     Credential: true
 }))
 app.use(express.json({ limit: "16kb"}))
@@ -15,5 +15,11 @@ app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser());
 
+
+// routes
+
+import userRouter from './routes/user.routes.js'
+
+app.use('/api/v1/users', userRouter);
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 // require('dotenv').config()
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({
     path: './.env'
@@ -9,8 +10,8 @@ dotenv.config({
 
 connectDB()
 .then( ()=>{
-     app.listen(Process.env.PORT || 8000, ()=>{
-          console.log(`Server is running at port: ${Process.env.PORT}`)
+     app.listen(process.env.PORT || 8000, ()=>{
+          console.log(`Server is running at port: ${process.env.PORT}`)
      })
 })
 .catch((err)=>{
